a6beb3470c630985401d1e28cc08f75261143807
everisbeca <everisbeca@ALC-EVERBECA>
2018-06-26 18:58:42 +0200

    leer escribir scanner

--- src/com/everis/alicante/courses/beca/garage/dao/AbstractGarageDAO.java
+++ src/com/everis/alicante/courses/beca/garage/dao/AbstractGarageDAO.java
@@ -66,3 +66,64 @@ public abstract class AbstractGarageDAO {
 		}
 	}
 }
+/*import java.io.File;
+import java.util.Scanner;
+
+public class LecturaFicheros {
+
+	public static void main(String[] args) {
+
+		// Fichero del que queremos leer
+		File fichero = new File("fichero_leer.txt");
+		Scanner s = null;
+
+		try {
+			// Leemos el contenido del fichero
+			System.out.println("... Leemos el contenido del fichero ...");
+			s = new Scanner(fichero);
+
+			// Leemos linea a linea el fichero
+			while (s.hasNextLine()) {
+				String linea = s.nextLine(); 	// Guardamos la linea en un String
+				System.out.println(linea);      // Imprimimos la linea
+			}
+
+		} catch (Exception ex) {
+			System.out.println("Mensaje: " + ex.getMessage());
+		} finally {
+			// Cerramos el fichero tanto si la lectura ha sido correcta o no
+			try {
+				if (s != null)
+					s.close();
+			} catch (Exception ex2) {
+				System.out.println("Mensaje 2: " + ex2.getMessage());
+			}
+		}
+	}
+}
+import java.io.FileWriter;
+
+public class EscrituraFicheros {
+
+	public static void main(String[] args) {
+
+		String[] lineas = { "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "..." };
+
+		 FORMA 1 DE ESCRITURA 
+		FileWriter fichero = null;
+		try {
+
+			fichero = new FileWriter("fichero_escritura.txt");
+
+			// Escribimos linea a linea en el fichero
+			for (String linea : lineas) {
+				fichero.write(linea + "\n");
+			}
+
+			fichero.close();
+
+		} catch (Exception ex) {
+			System.out.println("Mensaje de la excepción: " + ex.getMessage());
+		}
+	}
+}*/
